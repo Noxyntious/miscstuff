b94e04e178b3d2986497656c319f7cc39c50b98e
Eri <eri@nijika.dev>
2024-09-30 11:32:34 +0200

    upd 3

--- cheatsheet.py
+++ cheatsheet.py
@@ -4,6 +4,7 @@ promennaStr = "text" # určí proměnnou s typem řetězec (string), a textem "t
 promennaInt = 3 # určí promennou s typem celého čísla (integer), a hodnotou 3.
 # DŮLEŽITÉ: v typu Int není možné pouzit desetinné číslo.
 promennaFloat = 3.0 # určí proměnnou s typem desetinného čísla (floating point), a hodnotou 3.0
+# Důležité: Jméno proměnné nesouvisí s typem, typ určuje zadaná hodnota.
 
 print("text") # napíše do konzole text v uvozovkách.
 
