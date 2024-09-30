4e04479d2999fff9c87cb1d155f5a4a93f6c07c4
Eri <eri@nijika.dev>
2024-09-30 11:31:11 +0200

    upd 2

--- cheatsheet.py
+++ cheatsheet.py
@@ -1,1 +1,9 @@
-# comment
+# komentář. text za znakem # nebude považován jako kód, ale text.
+
+promennaStr = "text" # určí proměnnou s typem řetězec (string), a textem "text"
+promennaInt = 3 # určí promennou s typem celého čísla (integer), a hodnotou 3.
+# DŮLEŽITÉ: v typu Int není možné pouzit desetinné číslo.
+promennaFloat = 3.0 # určí proměnnou s typem desetinného čísla (floating point), a hodnotou 3.0
+
+print("text") # napíše do konzole text v uvozovkách.
+
